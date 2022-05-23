0445f7926060724aae17db0a239cf414b52e85b6
unknown <daniel.duplinszki@gmail.com>
2022-05-23 11:07:31 +0200

    picture anim works

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const fccImages = {
+    "Tribute Page": {
+        image:
+        "https://dand16.github.io/CDN/FCC%20responsive%20tribute.png"
+    },
+    "Landing Page": {
+        image:
+        "https://dand16.github.io/CDN/FCC%20responsive%20landing%20page.png"
+    },
+    "Technical Documentation": {
+        image:
+        "https://dand16.github.io/CDN/FCC%20responsive%20documentation.png"
+    },
+    "Survey Form": {
+        image:
+        "https://dand16.github.io/CDN/FCC%20responsive%20survey.png"
+    }
+};
+
+const links = document.querySelectorAll(".fcc-anchors");
+const image = document.querySelector(".preview-pictures");
+
+links.forEach((link) => {
+    link.addEventListener("pointerenter", ({ target }) => {
+      const { textContent } = target;
+      image.src = fccImages[textContent].image;
+      image.classList.toggle("preview-animation")
+    });
+    link.addEventListener("pointerout", () => {
+        image.classList.toggle("preview-animation")
+    })
+});
+/*
 let previewTribute = document.getElementById('preview-tribute');
 let previewLanding = document.getElementById('preview-landing');
 let previewDoc = document.getElementById('preview-doc');
@@ -44,3 +77,4 @@ anchorArr.forEach((element) => {
         })
     });
 });
+*/
